1481cd2eb7a381a5e51e6ca9bc45bf0774c0f96e
Jolo Cansana <jolo@cansana.net>
2020-04-05 01:54:22 +0800

    Allowed user to add items to shopping cart

---
+++ assets/js/app.js
@@ -0,0 +1,201 @@
+$(document).ready(function(){
+    
+    // START: CATALOGUE
+
+    /**$(".item").hover(function () {
+        $(this).children('.card-body').children('.itemFooter').children(".itemDescription").css("display", "none");
+        $(this).children('.card-body').children('.itemFooter').children(".addToCart").css("display", "block");
+    }, function () {
+        $(this).children('.card-body').children('.itemFooter').children(".itemDescription").css("display", "block");
+        $(this).children('.card-body').children('.itemFooter').children(".addToCart").css("display", "none");
+    })**/
+
+    var prevItem = null;
+    class cartItem {
+        constructor(productno, name, price, quantity) {
+            this.productno = productno;
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+    var shoppingCartItems = [];
+
+    $(".itemPicture").click(function () {
+        currItem = $(this).parent().parent().parent();
+        currItem.children('.card-body').children('.itemFooter').children(".itemDescription").css("display", "none");
+        currItem.children('.card-body').children('.itemFooter').children(".addToCart").css("display", "block");
+
+        if(prevItem != null)
+        {
+            prevItem.children('.card-body').children('.itemFooter').children(".itemDescription").css("display", "block");
+            prevItem.children('.card-body').children('.itemFooter').children(".addToCart").css("display", "none");
+        }
+
+        if (prevItem != null && prevItem.data('value') == currItem.data('value'))
+        {
+            prevItem = null;
+        } else
+        {
+            prevItem = currItem;
+        }
+    });
+
+    function changeShoppingCart(productno, name, price, newQuantity)
+    {
+        var item = shoppingCartItems.find(item => productno == item.productno);
+        if (item != null && newQuantity != 0)
+        {
+            // If its part of the shopping cart, update cart
+            item.quantity = newQuantity;
+        }
+        else if (item != null && newQuantity == 0)
+        {
+            // If its part of the shopping cart but new quantity is 0, delete from cart
+            shoppingCartItems.splice(shoppingCartItems.findIndex(item => productno == item.productno))
+        }
+        else
+        {
+            // Item doesn't exist, push to cart
+            var newItem = new cartItem(productno, name, price, newQuantity);
+            shoppingCartItems.push(newItem);
+        }
+
+        $('#shoppingCartItems').empty();
+
+        var cartTotalPrice = 0;
+
+        for(x of shoppingCartItems)
+        {
+            var itemTotalPrice = x.price*x.quantity;
+            var htmlstring = 
+            `
+            <div class='d-flex itemShoppingCart container rounded border border-dark bg-white px-1 my-2'>
+                <div class='col-7 text-left'>
+                    <div class='row'>
+                        ${x.name}
+                    </div>
+                    <div class='row'>
+                        ${x.price} X ${x.quantity}
+                    </div>
+                </div>
+                <div class='col-5 d-flex justify-content-end float-right text-right itemPrice'>${itemTotalPrice}</div>
+            </div>
+            `;
+
+            $('#shoppingCartItems').append(htmlstring);
+            cartTotalPrice += itemTotalPrice;
+        }
+
+        $('.totalPrice').html(cartTotalPrice.toFixed(2));
+        
+    }
+
+    $(".add").click(function ()
+    {
+        var val = parseInt($(this).parent().siblings("input").val());
+        var productno = $(this).parents().filter(".item").data('value');
+        var name = $(this).parents().filter(".addToCart").siblings(".itemDescription").children(".itemName").html();
+        var price = parseInt($(this).parents().filter(".addToCart").siblings(".itemDescription").children(".itemPrice").data("value"));
+        val++;
+
+        changeShoppingCart(productno, name, price, val);
+
+        $(this).parent().siblings("input").val(val);
+        console.log(productno, shoppingCartItems)
+    });
+
+    $(".subtract").click(function ()
+    {
+        var val = parseInt($(this).parent().siblings("input").val());
+        if(val > 0)
+        {
+            var productno = $(this).parents().filter(".item").data('value');
+            var name = $(this).parents().filter(".addToCart").siblings(".itemDescription").children(".itemName").html();
+            var price = parseInt($(this).parents().filter(".addToCart").siblings(".itemDescription").children(".itemPrice").data("value"));
+            val--;
+
+            changeShoppingCart(productno, name, price, val);
+
+            $(this).parent().siblings("input").val(val);
+            console.log(productno, shoppingCartItems);
+        }
+    });
+
+    $(".quantity-field").change(function ()
+    {
+        var val = parseInt($(this).val());
+        if(val > 0)
+        {
+            var productno = $(this).parents().filter(".item").data('value');
+            var name = $(this).parents().filter(".addToCart").siblings(".itemDescription").children(".itemName").html();
+            var price = parseInt($(this).parents().filter(".addToCart").siblings(".itemDescription").children(".itemPrice").data("value"));
+
+            changeShoppingCart(productno, name, price, val);
+
+            $(this).parent().siblings("input").val(val);
+            console.log(productno, shoppingCartItems)
+        } 
+        else
+        {
+            $(this).parent().siblings("input").val(0);
+            console.log(productno, shoppingCartItems);
+        }
+    });
+
+    // END: CATALOGUE
+    
+    
+    // START: PROFILE
+
+    var num = $("#number").val();
+    var add1 = $("#add1").val();
+    var add2 = $("#add2").val();
+
+    $("#btnEdit").click(function(){
+        $(this).hide();
+        $("#number").prop("readonly", false);
+        $("#add1").prop("readonly", false);
+        $("#add2").prop("readonly", false);
+        $("#btnCancel").css("display","block");
+        $("#btnConfirm").css("display","block");
+    });
+
+    $("#btnCancel").click(function(){
+        $("#btnCancel").css("display","none");
+        $("#btnConfirm").css("display","none");
+        $("#btnEdit").show();
+        $("#number").val(num);
+        $("#add1").val(add1);
+        $("#add2").val(add2);
+        $("#error").text("");
+        $("#number").prop("readonly", true);
+        $("#add1").prop("readonly", true);
+        $("#add2").prop("readonly", true);
+    });
+
+    $("#btnConfirm").click(function(){
+
+        var num = $("#number").val();
+        console.log(num);
+        var regex = /^(09)\d{9}$/;
+        if( regex.test(num) == false )
+        {
+            $("#error").text("Must be in 11-digit form: 09XXXXXXXXX");
+        }
+        else
+        {
+            $("#error").text("");
+            $("#btnCancel").css("display","none");
+            $("#btnConfirm").css("display","none");
+            $("#btnEdit").show();
+            $("#number").prop("readonly", true);
+            $("#add1").prop("readonly", true);
+            $("#add2").prop("readonly", true);
+        }
+        
+    });
+
+    // END: PROFILE
+});
+
--- index.js
+++ index.js
@@ -35,11 +35,11 @@ app.get('/register', (req,res) => {
 app.get('/catalogue', (req,res) => {
     res.render('catalogue', {title: 'Pharmago',
     catalogue: [
-        {itemPicture: "kirkland-vitc.png", itemName: "Kirkland Vitamin-C", itemPrice: "200.00"},
-        {itemPicture: "kirkland-vitd.png", itemName: "Kirkland Vitamin-D", itemPrice: "300.00"},
-        {itemPicture: "kirkland-vite.png", itemName: "Kirkland Vitamin-E", itemPrice: "500.00"},
-        {itemPicture: "enervon-vitc.png", itemName: "Enervon Vitamin-C", itemPrice: "150.00"},
-        {itemPicture: "biogesic-paracetamol.png", itemName: "Biogesic Paracetamol", itemPrice: "300.00"},
+        {productno: "1", itemPicture: "kirkland-vitc.png", itemName: "Kirkland Vitamin-C", itemPrice: "200.00"},
+        {productno: "2", itemPicture: "kirkland-vitd.png", itemName: "Kirkland Vitamin-D", itemPrice: "300.00"},
+        {productno: "3", itemPicture: "kirkland-vite.png", itemName: "Kirkland Vitamin-E", itemPrice: "500.00"},
+        {productno: "4", itemPicture: "enervon-vitc.png", itemName: "Enervon Vitamin-C", itemPrice: "150.00"},
+        {productno: "5", itemPicture: "biogesic-paracetamol.png", itemName: "Biogesic Paracetamol", itemPrice: "300.00"},
     ]})
 })
 
@@ -50,6 +50,16 @@ app.get('/checkout', (req,res) => {
         {itemPicture: "kirkland-vitd.png", itemName: "Kirkland Vitamin-D", itemPrice: "300.00", quantity: "2"},
         {itemPicture: "kirkland-vite.png", itemName: "Kirkland Vitamin-E", itemPrice: "500.00", quantity: "2"},
     ],
+    fullName: "John Doe",
+    email: "john.doe@gmail.com",
+    contactNumber: "09175666242",
+    addressLine1: "123 Main St.",
+    addressLine2: "Manila City, Metro Manila"
+    })
+})
+
+app.get('/profile', (req,res) => {
+    res.render('profile', {title: 'Profile',
     fullName: "John Doe",
     email: "john.doe@gmail.com",
     contactNumber: "09175666242",
